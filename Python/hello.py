6dd8eb9b390bd22190addeb162f5bffe38723b4d
Ibeaka Godson <106949329+Warmarth@users.noreply.github.com>
2024-02-26 12:51:26 +0100

    initial  commit

---
+++ Python/hello.py
@@ -0,0 +1,6 @@
+name = str(input('what is your name? '))
+age = str(input('what is your age? '))
+location = str(input('what is your location? '))
+ 
+output = (f'Hello {name}, I am {age} years old and live I in {location}.')
+print(output)
