875394b206d290b5a6ed2075d1e3389469f07a0b
Ibeaka Godson <106949329+Warmarth@users.noreply.github.com>
2024-03-06 02:10:51 +0100

    introduction to Dart

--- Python/hello.py
+++
@@ -1,6 +0,0 @@
-name = str(input('what is your name? '))
-age = str(input('what is your age? '))
-location = str(input('what is your location? '))
- 
-output = (f'Hello {name}, I am {age} years old and live I in {location}.')
-print(output)
---
+++ Python/week1/hello.py
@@ -0,0 +1,6 @@
+name = str(input('what is your name? ').title())
+age = str(input('what is your age? ').title())
+location = str(input('what is your location? ').title())
+ 
+output = (f'Hello {name}, I am {age} years old and live I in {location}.')
+print(output)
