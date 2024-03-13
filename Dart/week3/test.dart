e5adbcbbf31e367e69cca2e036da45aad8a0ea29
Ibeaka Godson <106949329+Warmarth@users.noreply.github.com>
2024-03-13 12:51:58 +0100

    week 3 Assignment

---
+++ Dart/week3/test.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+void main() {
+  stdout.write('enter a number of choice? ');
+  var output = stdin.readLineSync();
+  if (output == null || output.isEmpty) {
+    print('no input given...?');
+  } else {
+    var evaluator = int.tryParse(output);
+    if (evaluator == null) {
+      print('Your number is invalid');
+    } else if (evaluator > 10) {
+      print("Your number is greater than 10");
+    } else if (evaluator < 10) {
+      print("Your number is less than 10");
+    } else {
+      print("Your number is equal to 10");
+    }
+  }
+}
